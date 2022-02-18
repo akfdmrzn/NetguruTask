7f65241f6b111e30e0f741b3b28d1a7126868eaf
Akif Demirezen <maddemirezen@gmail.com>
2022-02-18 13:18:12 +0300

    first commit project

---
+++ NetguruTask/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NetguruTask
+//
+//  Created by Akif Demirezen on 18.02.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
